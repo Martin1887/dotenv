51090f0bf03246a5ebae17792e16787c95fb354d
Martín Pozo <mpozo@protonmail.com>
2023-05-15 12:14:53 +0200

    feat(wezterm): Nerd version of Fira Code Retina

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -41,7 +41,5 @@ return {
 	},
 	use_fancy_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
-	font = wezterm.font({
-		family = "Fira Code Retina",
-	}),
+	font = wezterm.font("FiraCode Nerd Font Ret"),
 }
