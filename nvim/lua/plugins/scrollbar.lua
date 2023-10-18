81b37033d8c4491796fae817c703bc719ff16db0
Martín Pozo <mpozo@protonmail.com>
2023-10-18 13:09:32 +0200

    fix(nvim): scrollbar search fix and color set to orange

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -1,37 +1,37 @@
 return {
   {
     "petertriho/nvim-scrollbar",
-    config = function()
-      require("scrollbar").setup {
-        handlers = {
-          cursor = true,
-          diagnostic = true,
-          gitsigns = true,
-          handle = true,
-          search = true,
-          ale = false,
-        },
-      }
-    end,
+    opts = {
+      handlers = {
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true,
+        handle = true,
+        search = true,
+        ale = false,
+      },
+      marks = {
+        Search = { color = "#ee8715" },
+      },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require("gitsigns").setup()
       require("scrollbar.handlers.gitsigns").setup()
-    end
+    end,
   },
-  -- TODO: scrollbar not working, opened issues
   {
     "kevinhwang91/nvim-hlslens",
     config = function()
       -- require('hlslens').setup() is not required
       require("scrollbar.handlers.search").setup({
-          -- hlslens config overrides, nvim and hop keymaps defaults are better
+        -- hlslens config overrides, nvim and hop keymaps defaults are better
       })
     end,
     dependencies = {
-    "petertriho/nvim-scrollbar"
-    }
-  }
+      "petertriho/nvim-scrollbar",
+    },
+  },
 }
