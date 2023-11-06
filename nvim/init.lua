14ec296b92caa29ebdb512759b590d5221cfcda8
Martín Pozo <mpozo@protonmail.com>
2023-11-06 18:05:31 +0100

    feat(nvim): save and close session before loading a new one

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,17 @@ vim.cmd("hi NeoTreeGitUntracked guifg=#eebb15")
 -- codespell with nvim-lint for specified and all filetypes
 vim.cmd('autocmd BufEnter,BufWritePost,InsertLeave * lua require("lint").try_lint()')
 vim.cmd('autocmd BufEnter,BufWritePost,InsertLeave * lua require("lint").try_lint({"codespell"})')
+
+local group = vim.api.nvim_create_augroup("PersistedHooks", {})
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "PersistedTelescopeLoadPre",
+  group = group,
+  callback = function(session)
+    -- Save the currently loaded session
+    require("persisted").save({ session = vim.g.persisted_loaded_session })
+
+    -- Delete all of the open buffers
+    vim.api.nvim_input("<ESC>:%bd!<CR>")
+  end,
+})
