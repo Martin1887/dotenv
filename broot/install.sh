5b7617a59c9abb0c8c1cad6fbdcf675bbfed7da7
Martín Pozo <mpozo@protonmail.com>
2024-03-02 12:59:08 +0100

    feat(broot): drill-down broot via ctrl+b

--- broot/install.sh
+++ broot/install.sh
@@ -6,6 +6,10 @@ fi
 if [ -f $HOME/.config/broot/verbs.hjson ]; then
 	mv $HOME/.config/broot/verbs.hjson $HOME/.config/broot/verbs.hjson.bak
 fi
+if [ -f $HOME/.config/broot/select-folder.hjson ]; then
+	mv $HOME/.config/broot/select-folder.hjson $HOME/.config/broot/select-folder.hjson.bak
+fi
 
 ln -s $HOME/workspace/dotenv/broot/conf.hjson $HOME/.config/broot/conf.hjson
 ln -s $HOME/workspace/dotenv/broot/verbs.hjson $HOME/.config/broot/verbs.hjson
+ln -s $HOME/workspace/dotenv/broot/select-folder.hjson $HOME/.config/broot/select-folder.hjson
--- zsh/external/broot.zsh
+++ zsh/external/broot.zsh
@@ -17,3 +17,31 @@ br () {  # [<broot-opt>...]
     return
   }
 }
+
+# -- Refresh prompt, rerunning any hooks --
+# Credit: Roman Perepelitsa
+# Original: https://github.com/romkatv/zsh4humans/blob/v2/fn/-z4h-redraw-prompt
+.zle_redraw-prompt () {
+  for 1 ( chpwd $chpwd_functions precmd $precmd_functions ) {
+    if (( $+functions[$1] ))  $1 &>/dev/null
+  }
+  zle .reset-prompt
+  zle -R
+}
+zle -N .zle_redraw-prompt
+
+# -- Folder Navigation: Down --
+# Key: ctrl+b
+# Depends: ~/.config/broot/select-folder.hjson
+# Optional: .zle_redraw-prompt
+.zle_cd-broot () {
+  echoti rmkx
+  cd "$(
+    <$TTY broot --color yes --git-ignored --show-git-info --only-folders \
+    --conf "${XDG_CONFIG_HOME:-${HOME}/.config}/broot/select-folder.hjson;${XDG_CONFIG_HOME:-${HOME}/.config}/broot/conf.hjson"
+  )"
+  if (( $+functions[.zle_redraw-prompt] ))  zle .zle_redraw-prompt
+  if (( $+functions[_zsh_highlight]     ))  _zsh_highlight
+}
+zle -N            .zle_cd-broot
+bindkey "^B" .zle_cd-broot  # alt+down
