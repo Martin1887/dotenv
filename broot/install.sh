d388c04fe26bbc8ad41a531c6125019b901ab0dd
Martín Pozo <mpozo@protonmail.com>
2024-03-02 18:44:30 +0100

    feat(broot): search paths (full complicated way) with CTRL+F

--- broot/install.sh
+++ broot/install.sh
@@ -9,7 +9,11 @@ fi
 if [ -f $HOME/.config/broot/select-folder.hjson ]; then
 	mv $HOME/.config/broot/select-folder.hjson $HOME/.config/broot/select-folder.hjson.bak
 fi
+if [ -f $HOME/.config/broot/select.hjson ]; then
+	mv $HOME/.config/broot/select.hjson $HOME/.config/broot/select.hjson.bak
+fi
 
 ln -s $HOME/workspace/dotenv/broot/conf.hjson $HOME/.config/broot/conf.hjson
 ln -s $HOME/workspace/dotenv/broot/verbs.hjson $HOME/.config/broot/verbs.hjson
 ln -s $HOME/workspace/dotenv/broot/select-folder.hjson $HOME/.config/broot/select-folder.hjson
+ln -s $HOME/workspace/dotenv/broot/select.hjson $HOME/.config/broot/select.hjson
--- zsh/external/broot.zsh
+++ zsh/external/broot.zsh
@@ -30,8 +30,8 @@ br () {  # [<broot-opt>...]
 }
 zle -N .zle_redraw-prompt
 
-# -- Folder Navigation: Down --
-# Key: ctrl+b
+# -- Folder Navigation --
+# Key: ctrl+B
 # Depends: ~/.config/broot/select-folder.hjson
 # Optional: .zle_redraw-prompt
 .zle_cd-broot () {
@@ -45,3 +45,85 @@ zle -N .zle_redraw-prompt
 }
 zle -N            .zle_cd-broot
 bindkey "^B" .zle_cd-broot  # alt+down
+
+
+# -- Complete current word as path using broot --
+# Key: ctrl+F
+# Depends: ~/.config/broot/select.hjson
+.zle_insert-path-broot () {
+  setopt localoptions extendedglob
+
+  # If the current word is partially typed, grab it (partial),
+  # pop it off the line, and create a search string for it (broot_search)
+  local partial broot_search
+  if [[ ${LBUFFER[-1]/ } ]] {
+
+    # Store it (partial)
+    partial=(${(z)LBUFFER})
+    partial=${partial[-1]}
+
+    # Remove it from the line
+    LBUFFER=${LBUFFER[1,-$#partial-1]}
+
+    # Expand home folder
+    partial=${partial/#(\$HOME\/|\~\/)/~\/}
+
+    # Store a sanitized version for broot search (broot_search)
+    broot_search=${partial/#~\/}                 # strip leading '~/'
+    broot_search=${broot_search//( |:|..\/|\;)}  # strip ' ', ':', '../', ';'
+    broot_search=${broot_search//.\/}            # strip './'
+    broot_search=${broot_search//\//\\/}         # replace '/' with '\/'
+
+  }
+
+  # If resulting search string starts with '.' or ends with '/.',
+  # search hidden files (can be slow)
+  local show_hidden=--no-hidden
+  if [[ $broot_search[1] == . || $broot_search[-2,-1] == /. ]]  show_hidden=--hidden
+
+  # If partial has leading '/', '../', or ~/,
+  # set broot start folder (start_dir)
+  local start_dir
+  if [[ $partial == ~/* ]] {
+    start_dir=~
+  } else {
+    local updirs=${(M)partial##(../|/)##}
+    start_dir=${updirs:-.}
+  }
+
+  # Get a location (or not) from broot,
+  # using select.hjson config
+  echoti rmkx
+  local locations=(${(f)"$(
+    <$TTY broot --color yes --cmd "$broot_search" $show_hidden --no-sizes \
+    --conf "${XDG_CONFIG_HOME:-${HOME}/.config}/broot/select.hjson;${XDG_CONFIG_HOME:-${HOME}/.config}/broot/conf.hjson" \
+    $start_dir
+  )"})
+
+  # Add location to line or restore original partial
+  if [[ $locations ]] {
+
+    # If GNU coreutils realpath is present,
+    # convert some paths from absolute to relative
+    if (( $+commands[realpath] )) {
+      if [[ $(realpath --help 2>&1) == *--relative-base* ]] {
+        local locations_abs=($locations) loc
+        locations=()
+        for loc ( $locations_abs )  locations+=("$(realpath --relative-base ~ --relative-to . $loc)")
+      }
+    }
+
+    # Ensure minimal and sufficient quoting
+    locations=(${(q-)locations})
+
+    LBUFFER+="$locations "
+  } else {
+    LBUFFER+=$partial
+  }
+
+  # If using fast-syntax-highlighting, retrigger it
+  if (( $+functions[_zsh_highlight] ))  _zsh_highlight
+
+}
+zle -N       .zle_insert-path-broot
+bindkey "^F" .zle_insert-path-broot  # ctrl+F
