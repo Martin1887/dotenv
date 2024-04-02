782bc5a2a273b84a8941038d1061b4521903b5f8
Martín Pozo <mpozo@protonmail.com>
2024-04-02 09:21:03 +0200

    feat(nvim): prev and next hunk keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,11 @@
 return {
   { "rhysd/git-messenger.vim" },
-  { "airblade/vim-gitgutter" },
+  {
+    "airblade/vim-gitgutter",
+    keys = {
+      { "[h", "<cmd>GitGutterPrevHunk<cr>", desc = "Previous hunk (git change)" },
+      { "]h", "<cmd>GitGutterNextHunk<cr>", desc = "Next hunk (git change)" },
+    },
+  },
   { "sindrets/diffview.nvim" },
 }
