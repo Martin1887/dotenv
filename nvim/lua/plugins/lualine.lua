81fb0325ec2314a33f163084a05136951e29c3ae
Martín Pozo <mpozo@protonmail.com>
2024-07-06 11:36:41 +0200

    feat(nvim): winbar with pretty full path and navic location

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,6 +8,10 @@ local function getLines()
   return tostring(vim.fn.line("$")) .. " lines"
 end
 
+local function navicLocation()
+  return require("nvim-navic").get_location()
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
@@ -15,6 +19,19 @@ return {
       theme = "melange",
     },
     sections = {
+      -- the path (with symbols navigation) is put in winbar instead
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+      },
       lualine_y = {
         { "filetype", icon_only = true, separator = "", padding = { left = 0, right = 1 } },
         { "encoding" },
@@ -24,6 +41,18 @@ return {
         { "progress", separator = "", padding = { left = 2, right = 1 } },
         { "location", padding = { left = 1, right = 1 } },
       },
+    },
+    winbar = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path({ length = 20 }) },
+        { navicLocation },
+      },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
     },
   },
 }
